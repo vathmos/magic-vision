83b229d1ade0daeb346de9ec73c7cd5ee7c828ed
vathmos <esqeue@gmail.com>
2025-04-08 21:03:46 +0700

    HeroUI setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import "@/styles/globals.css";
+import { HeroUIProvider } from "@heroui/react";
 import type { AppProps } from "next/app";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <HeroUIProvider>
+      <NextThemesProvider attribute="class" defaultTheme="dark" themes={["dark","light"]}>
+        <Component {...pageProps} />
+      </NextThemesProvider>
+    </HeroUIProvider>
+  )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+import {heroui} from "@heroui/react";
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  
+  theme: {
+    extend: {},
+  },
+  darkmode: "class",
+  plugins: [heroui()],
+}
+
