5a677c1e139b535f8be370813199c68f8c21899c
vathmos <esqeue@gmail.com>
2025-04-26 14:28:52 +0700

    add new logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
-import { Eye } from "lucide-react";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <div className="flex h-screen w-screen items-center justify-center">
-        <Eye className="size-96"></Eye>
+      <div className="flex flex-col h-screen w-screen items-center justify-center">
+        <Image src="/magic-vision.svg" alt="magic vision icon" width={400} height={0}/>
+        <h1 className="text-8xl">COMING SOON!</h1>
+
       </div>
     </>
   );
