47053059e056fd6a5a16fb79323e855546e76b09
Ahmad Faris Widyawan <esqeue@gmail.com>
2025-12-23 10:59:32 +0700

    improve SEO

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { HeroUIProvider } from "@heroui/react";
 import type { AppProps } from "next/app";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { Fraunces, Space_Grotesk } from "next/font/google";
+import Head from "next/head";
 
 const displayFont = Fraunces({
   subsets: ["latin"],
@@ -20,6 +21,9 @@ export default function App({ Component, pageProps }: AppProps) {
       <HeroUIProvider>
         <NextThemesProvider attribute="class" defaultTheme="dark" themes={["dark", "light"]}>
           <div className={`${displayFont.variable} ${bodyFont.variable} min-h-screen`}>
+            <Head>
+              <title>Magic Vision</title>
+            </Head>
             <Component {...pageProps} />
           </div>
         </NextThemesProvider>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head/>
+      <Head>
+        <meta
+          name="description"
+          content="Magic Vision is a matchmaking predictor for Magic Chess: GO GO. Build the order once, then track your next opponent."
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
